36ce36b6828299d9cad1a9fc435e1badecf313f7
madhu2852 <madhu2852@users.noreply.github.com>
2021-08-24 17:15:04 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,8 +35,8 @@ resource "aws_lb_listener_rule" "public_alb_lstnr_rule_app" {
 
 ### CREATE TARGET GROUP ATTACHMENTS FOR THE APP ###
 
-# resource "aws_lb_target_group_attachment" "public_alb_app_tg" {
-#   count = length(data.aws_instances.public_alb_palo_alto)
-#   target_group_arn = aws_lb_target_group.public_alb_app[count.index].arn
-#   target_id        = data.aws_instances.public_alb_palo_alto[count.index].id
-# }
+resource "aws_lb_target_group_attachment" "public_alb_app_tg" {
+  count = length(data.aws_instances.public_alb_palo_alto.ids)
+  target_group_arn = aws_lb_target_group.public_alb_app.arn
+  target_id        = data.aws_instances.public_alb_palo_alto.ids[count.index]
+}
