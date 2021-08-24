c1b62a4c6ce464933056ce4fc430846034cf4aef
madhu2852 <madhu2852@users.noreply.github.com>
2021-08-24 14:59:06 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+### CREATE TARGET GROUP FOR THE APP ###
+
+resource "aws_lb_target_group" "public_alb_app" {
+  name     = "pub-alb-app-${var.APP_FQDN}"
+  port     = var.available_port
+  protocol = "HTTP"
+  target_type = "instance"
+  vpc_id   = data.aws_vpc.ingress_vpc_id.id # use data resource to pull the info
+  health_check {
+    port = var.available_port
+    protocol = "HTTP"
+    interval = 30
+    timeout = 20
+    unhealthy_threshold = 10
+  }
+}
+
+### CREATE LISTENER RULE FOR THE APP ###
+
+resource "aws_lb_listener_rule" "public_alb_lstnr_rule_app" {
+  listener_arn = var.LISTENER_ARN
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.public_alb_app.arn
+  }
+  condition {
+    host_header {
+      values = [var.APP_FQDN]
+    }
+  }
+}
+
+### CREATE TARGET GROUP ATTACHMENTS FOR THE APP ###
+
+# resource "aws_lb_target_group_attachment" "public_alb_app_tg" {
+#   count = length(data.aws_instances.public_alb_palo_alto)
+#   target_group_arn = aws_lb_target_group.public_alb_app[count.index].arn
+#   target_id        = data.aws_instances.public_alb_palo_alto[count.index].id
+# }
