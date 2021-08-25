dbd98aea960af4201abb34f02bb2a7941edb65c7
madhu2852 <madhu2852@users.noreply.github.com>
2021-08-25 09:36:45 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_lb_target_group" "public_alb_app" {
 ### CREATE LISTENER RULE FOR THE APP ###
 
 resource "aws_lb_listener_rule" "public_alb_lstnr_rule_app" {
-  listener_arn = var.PUBLIC_ALB_LSTNR_NAME
+  listener_arn = data.aws_resourcegroupstaggingapi_resources.load_balancer_lstnr.resource_tag_mapping_list[0].resource_arn
   action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.public_alb_app.arn
