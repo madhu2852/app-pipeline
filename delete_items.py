12e47a5ca79ad7d82c35c0a73fe276b2f3380040
madhu2852 <madhu2852@users.noreply.github.com>
2021-08-31 16:52:50 -0500

    Update delete_items.py

--- delete_items.py
+++ delete_items.py
@@ -1,77 +1,88 @@
-import boto3
-from boto3.dynamodb.conditions import Key, Attr
-from optparse import OptionParser
-import logging
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurperClassic
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+pipeline {
+    agent any
+    options {
+        timestamps()
+        buildDiscarder(logRotator(daysToKeepStr: '90'))
+        disableConcurrentBuilds()
+    }
+    environment {
+        
+        AWS_ACCESS_KEY_ID = credentials('secret-key-id')
+        AWS_SECRET_ACCESS_KEY = credentials('access-key')
+        PIP_ENV = """${sh(
+                    returnStdout: true,
+                    script: '''
+                        set +x
+                        export PATH=~/.local/bin:$PATH
+                        pip3 install pipenv --user > /dev/null
+                        pipenv update > /dev/null
+                        pipenv install boto3 > /dev/null
+                    '''    
+                        ).trim()}"""   
+    }
+    parameters {
+        string(
+            name: 'APP_FQDN',
+            description: 'application fqdn',
+            trim: true,
+        )
+        string(
+            name: 'REGION',
+            description: 'application fqdn',
+            trim: true,
+            defaultValue: 'us-east-1'
+        )
 
-def get_configs():
-    parser = OptionParser()
-    parser.add_option("--listener_rule_arn", "--listener_rule_arn",dest="listener_rule_arn",help="fqdn of the app to remove",default=None)
-    parser.add_option("--target_group_arn", "--target_group_arn",dest="target_group_arn",help="dynamodb region",default=None)
-    parser.add_option("--region", "--region",dest="region",help="dynamodb region",default=None)
-    parser.add_option("--portnum", "--portnum",dest="portnum",help="assigned port number",default=None)
-    # parser.add_option("--fqdn", "--fqdn",dest="fqdn",help="fqdn of the application",default=None)
-    parser.add_option("--table_name", "--table_name",dest="table_name",help="dynamodb table name to query",default=None)
+    }
+    stages {        
+        stage('Preparation') { // GET THE LOAB BALANCER NAME, PORT and, CERT TO DELETE
 
-
-    (options, args) = parser.parse_args()
-    try:
-        options.map = json.loads(options.map)
-    except:
-        options.map = None
-    return options
-
-try:
-    client = boto3.client('elbv2', region_name='us-east-1')
-except Exception as e:
-    message = 'FAILED: Unable to establish connection to ELB - {}'.format(e)
-    raise Exception(message) 
-
-def delete_aws_resources(listener_rule_arn,target_group_arn):
-    delete_lstnr_rule = client.delete_rule(
-        RuleArn=str(listener_rule_arn)
-    )
-    delete_tg_grp = client.delete_target_group(
-        TargetGroupArn=str(target_group_arn)
-    )
-    return delete_lstnr_rule,delete_tg_grp
-
-
-def update_ddb_ssm(region,table_name,portnum):
-
-    try:
-        ddb_table = boto3.resource('dynamodb',region_name=region).Table(table_name)
-    except Exception as e:
-        raise Exception(e)
-
-
-    update_ddb = ddb_table.update_item(
-        Key={
-            'portnum': portnum
-        },
-        UpdateExpression=("SET port_state = :s REMOVE fqdn"),
-        ExpressionAttributeValues={
-            ':s': 'a'
-        },
-    )
-    return update_ddb
-
-def main():
-
-    global options
-    options = get_configs()
-
-    try:
-        remove_aws = delete_aws_resources(options.listener_rule_arn,options.target_group_arn)
-        update_metadata_ddb_ssm = update_ddb_ssm(options.region,options.table_name,options.portnum)
-
-        return remove_aws,update_metadata_ddb_ssm
-
-    except Exception as e:
-        message = 'FAILED: Script Failed to Execute {}'.format(e)
-        return message
-
-if __name__ == "__main__":
-     print(main())
+            environment {
+                RAW_DATA = """${sh(
+                    returnStdout: true,
+                    script: '''
+                        set +x
+                        export PATH=~/.local/bin:$PATH
+                        pipenv run python3 ./get_item.py --region=${REGION} --fqdn=${APP_FQDN}
+                    '''    
+                        ).trim()}"""
+                }
+            steps {
+                script {
+                 def jsonObj = readJSON text: RAW_DATA
+                 env.ELB =  jsonObj.elb
+                 env.APP_ENVIRONMENT = jsonObj.env
+                 env.CERT = jsonObj.cert
+                 env.PORT = jsonObj.port
+                 env.LISTENER_RULE_ARN = jsonObj.lstnr_rule_arn
+                 env.TG_GROUP_ARN = jsonObj.target_grp_arn
+                 env.FQDN = jsonObj.fqdn
+                 env.DDB_TABLE = jsonObj.table_name
+                }
+            }
+        } 
+        stage('Cleanup AWS Resources') {            
+            steps {
+                sh '''
+                    export PATH=~/.local/bin:$PATH
+                    pipenv run python3 ./delete_items.py \
+                        --listener_rule_arn=${LISTENER_RULE_ARN} \
+                        --target_group_arn=${TG_GROUP_ARN} \
+                        --portnum=${PORT} \
+                        --region=${REGION} \
+                        --table_name=${DDB_TABLE} \
+                        --json_data=${RAW_DATA}
+                        --fqdn=${FQDN}
+                 '''
+                }
+            }
+        }
+    post {
+        cleanup {
+            cleanWs(cleanWhenAborted: false)
+        }
+    }    
+}
