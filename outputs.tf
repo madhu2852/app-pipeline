ac508800af71b724801c41e7069b928c9d7fa598
madhu2852 <madhu2852@users.noreply.github.com>
2021-08-31 10:58:21 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,9 @@ output "public_elb_lstnr" {
 output "internal_app_cert" {
     value = data.aws_resourcegroupstaggingapi_resources.internal_cert.resource_tag_mapping_list.*.resource_arn
 }
+output "lstnr_rule_arn" {
+    value = aws_lb_listener_rule.public_alb_lstnr_rule_app.arn
+}
+output "target_group_arn" {
+    value = aws_lb_target_group.public_alb_app.arn
+}
