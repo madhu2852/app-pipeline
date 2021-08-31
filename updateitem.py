95f4ff54cf188de76da4add0bd134f99b1e4fe75
madhu2852 <madhu2852@users.noreply.github.com>
2021-08-31 11:39:11 -0500

    Update updateitem.py

--- updateitem.py
+++ updateitem.py
@@ -2,6 +2,7 @@ import boto3
 from boto3.dynamodb.conditions import Key, Attr
 from optparse import OptionParser
 import logging
+import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -12,7 +13,11 @@ def get_configs():
     parser.add_option("--region", "--region",dest="region",help="dynamodb region",default=None)
     parser.add_option("--table_name", "--table_name",dest="table_name",help="dynamodb table name to query",default=None)
     parser.add_option("--portnum", "--portnum",dest="portnum",help="assigned port number",default=None)
-    parser.add_option("--fqdn", "--fqdn",dest="fqdn",help="fqdn of application",default=None)
+    parser.add_option("--fqdn", "--fqdn",dest="fqdn",help="fqdn of the application",default=None)
+    parser.add_option("--lstnr_rule_arn", "--lstnr_rule_arn",dest="lstnr_rule_arn",help="listener rule of the application",default=None)
+    parser.add_option("--cert", "--cert",dest="cert",help="cert of the application",default=None)
+    parser.add_option("--alb", "--alb",dest="alb",help="alb used for the application",default=None)
+    parser.add_option("--target_grp_arn", "--target_grp_arn",dest="target_grp_arn",help="target group of the application",default=None)
 
     (options, args) = parser.parse_args()
     try:
@@ -22,7 +27,7 @@ def get_configs():
     return options
 
 
-def update_metadata(region,table_name,portnum,fqdn):
+def update_metadata(region,table_name,portnum,fqdn,lstnr_rule_arn,cert,env,alb,target_grp_arn):
     try:
         table = boto3.resource('dynamodb',region_name=region).Table(table_name)
         client = boto3.client('ssm',region_name=region)
@@ -40,10 +45,18 @@ def update_metadata(region,table_name,portnum,fqdn):
         },
         ReturnValues="UPDATED_NEW"
     )
+    json_data = { 
+            "port": portnum, 
+            "lstnr_rule_arn": lstnr_rule_arn,
+            "cert": cert,
+            "environment": env,
+            "alb": alb,
+            "target_grp_arn": target_grp_arn,
+        }
     response = client.put_parameter(
         Name=fqdn,
-        Value=portnum,
-        Type='String'
+        Value=json.dumps(json_data),
+        Type='String',
     )
     return update,response
 
@@ -53,7 +66,15 @@ def main():
     options = get_configs()
 
     try:
-        metadata = update_metadata(options.region,options.table_name,options.portnum,options.fqdn)
+        metadata = update_metadata(options.region,
+            options.table_name,
+            options.portnum,
+            options.fqdn,
+            options.alb,
+            options.cert,
+            options.target_grp_arn,
+            options.env,
+            options.lstnr_rule_arn)
         message = "Successfully updated metadata to Dynamodb and SSM"
         logger.info(message)
         return metadata
