7beff512bdea2f4653d40108c6dba9c1e57fb1e2
madhu2852 <madhu2852@users.noreply.github.com>
2021-09-01 09:05:43 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,3 +40,10 @@ resource "aws_lb_target_group_attachment" "public_alb_app_tg" {
   target_group_arn = aws_lb_target_group.public_alb_app.arn
   target_id        = data.aws_instances.public_alb_palo_alto.ids[count.index]
 }
+
+### ADD APP SPECIFIC CERTS ###
+
+resource "aws_lb_listener_certificate" "app_internal_cert" {
+  listener_arn  = data.aws_resourcegroupstaggingapi_resources.load_balancer_lstnr.resource_tag_mapping_list[0].resource_arn
+  certificate_arn = data.aws_resourcegroupstaggingapi_resources.internal_cert.resource_tag_mapping_list[0].resource_arn
+}
