7c9542de1bbd8f7e7a908eeda727e5213f0924b8
madhu2852 <madhu2852@users.noreply.github.com>
2021-09-13 15:41:40 -0500

    Update delete_items.py

--- delete_items.py
+++ delete_items.py
@@ -15,10 +15,9 @@ def get_configs():
     parser.add_option("--portnum", "--portnum",dest="portnum",help="assigned port number",default=None)
     parser.add_option("--fqdn", "--fqdn",dest="fqdn",help="fqdn of the application",default=None)
     parser.add_option("--table_name", "--table_name",dest="table_name",help="dynamodb table name to query",default=None)
-    parser.add_option("--json_data", "--json_data",dest="json_data",help="json data from ssm param store",default=None)
     parser.add_option("--cert_arn", "--cert_arn",dest="cert_arn",help="app cert arn",default=None)
     parser.add_option("--listener_arn", "--listener_arn",dest="listener_arn",help="listener arn",default=None)
-
+    parser.add_option("--state", "--state",dest="state",help="state of app fqdn",default=None)
 
     (options, args) = parser.parse_args()
     try:
@@ -56,33 +55,47 @@ def delete_aws_resources(listener_rule_arn,target_group_arn,listener_arn,cert_ar
 
 
 
-def update_ddb_ssm(region,table_name,portnum,fqdn,json_data):
+def update_ddb_ssm(region,table_name,portnum,fqdn,alb,cert,target_grp_arn,env,lstnr_rule_arn,listener_arn,state):
 
     try:
         ddb_table = boto3.resource('dynamodb',region_name=region).Table(table_name)
         ssm_client = boto3.client('ssm',region_name=region)
+        json_data = {
+            "region": region,
+            "table_name": table_name,
+            "port": portnum,
+            "fqdn": fqdn,
+            "alb": alb,
+            "cert": cert,
+            "target_grp_arn": target_grp_arn,
+            "environment": env,
+            "lstnr_rule_arn": lstnr_rule_arn,
+            "listener_arn": listener_arn,
+            "state": state,
+        }
+
+        update_ddb = ddb_table.update_item(
+            Key={
+                'portnum': portnum
+            },
+            UpdateExpression=("SET port_state = :s REMOVE fqdn"),
+            ExpressionAttributeValues={
+                ':s': 'a'
+            },
+        )
+
+        update_ssm = ssm_client.put_parameter(
+            Name=fqdn,
+            Description='not-in-use',
+            Value=json.dumps(json_data),
+            Overwrite=True,
+            Type='String',
+        )
+        return update_ddb,update_ssm
+
     except Exception as e:
         raise Exception(e)
 
-
-    update_ddb = ddb_table.update_item(
-        Key={
-            'portnum': portnum
-        },
-        UpdateExpression=("SET port_state = :s REMOVE fqdn"),
-        ExpressionAttributeValues={
-            ':s': 'a'
-        },
-    )
-
-    update_ssm = ssm_client.put_parameter(
-        Name=fqdn,
-        Description='not-in-use',
-        Value=json_data,
-        Overwrite=True,
-        Type='String',
-    )
-    return update_ddb
 
 def main():
 
@@ -101,7 +114,7 @@ def main():
             options.table_name,
             options.portnum,
             options.fqdn,
-            options.json_data
+            options.state
             )
 
         return remove_aws,update_metadata_ddb_ssm
