4bfcc5ce4678c2ce702076ef205f2124e2c3422f
madhu2852 <madhu2852@users.noreply.github.com>
2021-09-13 16:22:23 -0500

    Update get_item.py

--- get_item.py
+++ get_item.py
@@ -1,7 +1,5 @@
 import boto3
-import os
 from boto3.dynamodb.conditions import Key, Attr
-from botocore.exceptions import ClientError
 from optparse import OptionParser
 import logging
 
@@ -12,8 +10,6 @@ def get_configs():
     parser = OptionParser()
     parser.add_option("--fqdn", "--fqdn",dest="fqdn",help="fqdn of the app to remove",default=None)
     parser.add_option("--region", "--region",dest="region",help="dynamodb region",default=None)
-    parser.add_option("--check", "--check",dest="check",help="check if app fqdn already exists",default=None)
-
     (options, args) = parser.parse_args()
     try:
         options.map = json.loads(options.map)
@@ -23,55 +19,15 @@ def get_configs():
 
 
 def get_parameters(region,fqdn):
-    try:
-        ssm = boto3.client('ssm', region_name=region)
-        response = ssm.get_parameters(
-            Names=[
-                str(fqdn),
-            ],
-            WithDecryption=True
-        )
-        return response['Parameters'][0]['Value']
-    except Exception as e:
-        message = 'FAILED: Script Failed to connect to SSM or Input Parameter Not Found {}'.format(e)
-        sys.exit(2)
-        return message
+    ssm = boto3.client('ssm', region_name=region)
+    response = ssm.get_parameters(
+        Names=[
+            fqdn,
+        ],
+        WithDecryption=True
+    )
+    return response['Parameters'][0]['Value']
 
-## Check APP FQDN already in use. If in use exit.
-
-def fqdn_verify(region,fqdn):
-    try:
-        ssm = boto3.client('ssm', region_name=region)
-        response = ssm.get_parameters(
-            Names=[
-                str(fqdn),
-            ],
-            WithDecryption=True
-        )
-        json_data = response['Parameters'][0]['Value']
-        print(json_data)
-        fqdn_state = json_data['State']
-        if fqdn_state == "in-use":
-            message = 'Match Found: FQDN is already in use. Exiting....'
-            print(message)
-            sys.exit(2)
-        return response['Parameters'][0]['Value']
-    except ssm.exceptions.ParameterNotFound as e:
-        message = 'Success: Parameter Not Found. Available to use. Proceeding.... {}'.format(e)
-        print(message)
-        sys.exit(0)
-    except ssm.exceptions.InvalidKeyId as e:
-        message = 'FAILED: Invalid Key ID. Exiting.... {}'.format(e)
-        print(message)
-        sys.exit(2)
-    except ssm.exceptions.InternalServerError as e:
-        message = 'FAILED: Internal Server Error. Exiting.... {}'.format(e)
-        print(message)
-        sys.exit(2)
-    except ssm.exceptions.ParameterVersionNotFound as e:
-        message = 'FAILED: Parameter Version not found!!!. Exiting.... {}'.format(e)
-        print(message)
-        sys.exit(2)
 
 def main():
 
@@ -79,12 +35,11 @@ def main():
     options = get_configs()
 
     try:
-        if (options.check):
-            fqdn_validate  = fqdn_verify(options.region,options.fqdn)
-            return fqdn_validate
-        else:
-            metadata = get_parameters(options.region,options.fqdn)
-            return metadata
+        metadata = get_parameters(options.region,options.fqdn)
+        message = "Received data"
+        logger.info(message)
+        return metadata
+
     except Exception as e:
         message = 'FAILED: Script Failed to Execute {}'.format(e)
         return message
