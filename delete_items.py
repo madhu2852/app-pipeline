4a75ff41c2324228783d64b6e36294e557ff5715
madhu2852 <madhu2852@users.noreply.github.com>
2021-09-15 11:00:02 -0500

    Update delete_items.py

--- delete_items.py
+++ delete_items.py
@@ -15,9 +15,9 @@ def get_configs():
     parser.add_option("--portnum", "--portnum",dest="portnum",help="assigned port number",default=None)
     parser.add_option("--fqdn", "--fqdn",dest="fqdn",help="fqdn of the application",default=None)
     parser.add_option("--table_name", "--table_name",dest="table_name",help="dynamodb table name to query",default=None)
-    parser.add_option("--json_data", "--json_data",dest="json_data",help="json data from ssm param store",default=None)
     parser.add_option("--cert_arn", "--cert_arn",dest="cert_arn",help="app cert arn",default=None)
     parser.add_option("--listener_arn", "--listener_arn",dest="listener_arn",help="listener arn",default=None)
+    parser.add_option("--state", "--state",dest="state",help="state of the application",default=None)
 
 
     (options, args) = parser.parse_args()
@@ -28,7 +28,7 @@ def get_configs():
     return options
 
 try:
-    client = boto3.client('elbv2', region_name='us-east-1')
+    client = boto3.client('elbv2', region_name=options.region)
 except Exception as e:
     message = 'FAILED: Unable to establish connection to ELB - {}'.format(e)
     raise Exception(message) 
@@ -56,7 +56,7 @@ def delete_aws_resources(listener_rule_arn,target_group_arn,listener_arn,cert_ar
 
 
 
-def update_ddb_ssm(region,table_name,portnum,fqdn,json_data):
+def update_ddb_ssm(region,table_name,portnum,fqdn,alb,cert,target_grp_arn,env,lstnr_rule_arn,listener_arn,state): 
 
     try:
         ddb_table = boto3.resource('dynamodb',region_name=region).Table(table_name)
@@ -74,11 +74,24 @@ def update_ddb_ssm(region,table_name,portnum,fqdn,json_data):
             ':s': 'a'
         },
     )
+    json_data = {
+            "region": region,
+            "table_name": table_name,
+            "port": portnum,
+            "fqdn": fqdn,
+            "alb": alb,
+            "cert": cert,
+            "target_grp_arn": target_grp_arn,
+            "environment": env,
+            "lstnr_rule_arn": lstnr_rule_arn,
+            "listener_arn": listener_arn,
+            "state": state,
+        }
 
     update_ssm = ssm_client.put_parameter(
         Name=fqdn,
         Description='not-in-use',
-        Value=json_data,
+        Value=json.dumps(json_data),
         Overwrite=True,
         Type='String',
     )
@@ -101,7 +114,13 @@ def main():
             options.table_name,
             options.portnum,
             options.fqdn,
-            options.json_data
+            options.alb,
+            options.cert,
+            options.target_grp_arn,
+            options.env,
+            options.lstnr_rule_arn,
+            options.listener_arn,
+            options.state,
             )
 
         return remove_aws,update_metadata_ddb_ssm
