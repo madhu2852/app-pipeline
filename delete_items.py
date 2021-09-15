9b8d01ca0d09f44eae21dca6e7d48f4d4120f84f
madhu2852 <madhu2852@users.noreply.github.com>
2021-09-15 11:09:04 -0500

    Update delete_items.py

--- delete_items.py
+++ delete_items.py
@@ -27,75 +27,75 @@ def get_configs():
         options.map = None
     return options
 
-try:
-    client = boto3.client('elbv2', region_name=options.region)
-except Exception as e:
-    message = 'FAILED: Unable to establish connection to ELB - {}'.format(e)
-    raise Exception(message) 
+def delete_aws_resources(listener_rule_arn,target_group_arn,listener_arn,cert_arn,region):
 
-def delete_aws_resources(listener_rule_arn,target_group_arn,listener_arn,cert_arn):
+    try:
+        client = boto3.client('elbv2', region_name=region)
 
-    delete_lstnr_rule = client.delete_rule(
-        RuleArn=str(listener_rule_arn)
-    )
+        delete_lstnr_rule = client.delete_rule(
+            RuleArn=str(listener_rule_arn)
+        )
 
-    delete_tg_grp = client.delete_target_group(
-        TargetGroupArn=str(target_group_arn)
-    )
+        delete_tg_grp = client.delete_target_group(
+            TargetGroupArn=str(target_group_arn)
+        )
 
-    # remove_lstnr_cert = client.remove_listener_certificates(
-    #     ListenerArn=str(listener_arn),
-    #     Certificates=[
-    #         {
-    #             'CertificateArn': str(cert_arn),
-    #         },
-    #     ]
-    # )
+        # remove_lstnr_cert = client.remove_listener_certificates(
+        #     ListenerArn=str(listener_arn),
+        #     Certificates=[
+        #         {
+        #             'CertificateArn': str(cert_arn),
+        #         },
+        #     ]
+        # )
+        return delete_lstnr_rule,delete_tg_grp #,remove_lstnr_cert
 
-    return delete_lstnr_rule,delete_tg_grp #,remove_lstnr_cert
-
-
+    except Exception as e:
+        message = 'FAILED: Unable to remove objects from AWS. Reason: {}'.format(e)
+        raise Exception(message)     
 
 def update_ddb_ssm(region,table_name,portnum,fqdn,alb,cert,target_grp_arn,env,lstnr_rule_arn,listener_arn,state): 
 
     try:
         ddb_table = boto3.resource('dynamodb',region_name=region).Table(table_name)
         ssm_client = boto3.client('ssm',region_name=region)
+
+        update_ddb = ddb_table.update_item(
+            Key={
+                'portnum': portnum
+            },
+            UpdateExpression=("SET port_state = :s REMOVE fqdn"),
+            ExpressionAttributeValues={
+                ':s': 'a'
+            },
+        )
+        json_data = {
+                "region": region,
+                "table_name": table_name,
+                "port": portnum,
+                "fqdn": fqdn,
+                "alb": alb,
+                "cert": cert,
+                "target_grp_arn": target_grp_arn,
+                "environment": env,
+                "lstnr_rule_arn": lstnr_rule_arn,
+                "listener_arn": listener_arn,
+                "state": state,
+            }
+
+        update_ssm = ssm_client.put_parameter(
+            Name=fqdn,
+            Description='not-in-use',
+            Value=json.dumps(json_data),
+            Overwrite=True,
+            Type='String',
+        )
+
+        return update_ddb
+
     except Exception as e:
-        raise Exception(e)
-
-
-    update_ddb = ddb_table.update_item(
-        Key={
-            'portnum': portnum
-        },
-        UpdateExpression=("SET port_state = :s REMOVE fqdn"),
-        ExpressionAttributeValues={
-            ':s': 'a'
-        },
-    )
-    json_data = {
-            "region": region,
-            "table_name": table_name,
-            "port": portnum,
-            "fqdn": fqdn,
-            "alb": alb,
-            "cert": cert,
-            "target_grp_arn": target_grp_arn,
-            "environment": env,
-            "lstnr_rule_arn": lstnr_rule_arn,
-            "listener_arn": listener_arn,
-            "state": state,
-        }
-
-    update_ssm = ssm_client.put_parameter(
-        Name=fqdn,
-        Description='not-in-use',
-        Value=json.dumps(json_data),
-        Overwrite=True,
-        Type='String',
-    )
-    return update_ddb
+        message = 'FAILED: Unable to update SSM metadata. Reason: {}'.format(e)
+        raise Exception(message)
 
 def main():
 
@@ -107,7 +107,8 @@ def main():
             options.listener_rule_arn,
             options.target_group_arn,
             options.listener_arn,
-            options.cert_arn
+            options.cert_arn,
+            options.region,
             )
         update_metadata_ddb_ssm = update_ddb_ssm(
             options.region,
