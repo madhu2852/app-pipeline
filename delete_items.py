a240c65ea87d0dbcad05eb2ca7654a6c081296cc
madhu2852 <madhu2852@users.noreply.github.com>
2021-12-02 16:19:23 -0600

    Update delete_items.py

--- delete_items.py
+++ delete_items.py
@@ -1,53 +1,33 @@
 import boto3
+from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key, Attr
 from optparse import OptionParser
 import logging
 import json
+import sys
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def get_configs():
-    parser = OptionParser()
-    parser.add_option("--env", "--env",dest="env",help="environment to get the available port",default=None)
-    parser.add_option("--lstnr_rule_arn", "--lstnr_rule_arn",dest="lstnr_rule_arn",help="fqdn of the app to remove",default=None)
-    parser.add_option("--target_grp_arn", "--target_grp_arn",dest="target_grp_arn",help="dynamodb region",default=None)
-    parser.add_option("--region", "--region",dest="region",help="dynamodb region",default=None)
-    parser.add_option("--portnum", "--portnum",dest="portnum",help="assigned port number",default=None)
-    parser.add_option("--fqdn", "--fqdn",dest="fqdn",help="fqdn of the application",default=None)
-    parser.add_option("--table_name", "--table_name",dest="table_name",help="dynamodb table name to query",default=None)
-    parser.add_option("--cert", "--cert",dest="cert",help="app cert arn",default=None)
-    parser.add_option("--alb", "--alb",dest="alb",help="alb used for the application",default=None)
-    parser.add_option("--listener_arn", "--listener_arn",dest="listener_arn",help="listener arn",default=None)
-    parser.add_option("--state", "--state",dest="state",help="state of the application",default=None)
-
-
+    usage = "usage: %prog [options] arg"
+    parser = OptionParser(usage)
+    parser.add_option("--env", "--env",dest="env",help="environment to get the available port")
+    parser.add_option("--lstnr_rule_arn", "--lstnr_rule_arn",dest="lstnr_rule_arn",help="fqdn of the app to remove")
+    parser.add_option("--target_grp_arn", "--target_grp_arn",dest="target_grp_arn",help="dynamodb region")
+    parser.add_option("--region", "--region",dest="region",help="dynamodb region")
+    parser.add_option("--portnum", "--portnum",dest="portnum",help="assigned port number")
+    parser.add_option("--fqdn", "--fqdn",dest="fqdn",help="fqdn of the application")
+    parser.add_option("--table_name", "--table_name",dest="table_name",help="dynamodb table name to query")
+    parser.add_option("--cert", "--cert",dest="cert",help="app cert arn")
+    parser.add_option("--alb", "--alb",dest="alb",help="alb used for the application")
+    parser.add_option("--listener_arn", "--listener_arn",dest="listener_arn",help="listener arn")
+    parser.add_option("--state", "--state",dest="state",help="state of the application")
     (options, args) = parser.parse_args()
-    try:
-        options.map = json.loads(options.map)
-    except:
-        options.map = None
+
     return options
 
-def check_fqdn(fqdn,region):
-    try:
-        ssm_client = boto3.client('ssm',region_name=region)
 
-        response = ssm_client.get_parameter(
-            Name=str(fqdn),
-        )
-        data = response['Parameter']['Value']
-        json_data = json.loads(data)
-        app_state = json_data['state']
-        if app_state != 'in-use':
-            message = "objects cannot be deleted due to either the fqdn is not configured or already has been deleted."
-            raise Exception(message)
-        else:
-            return 'Object Found and can be deleted.'   
-    except Exception as e:
-        message = 'Reason - {}'.format(e)
-        raise Exception(message)
-        
 def delete_aws_resources(
     lstnr_rule_arn,
     target_grp_arn,
@@ -61,7 +41,6 @@ def delete_aws_resources(
         delete_lstnr_rule = client.delete_rule(
             RuleArn=str(lstnr_rule_arn)
         )
-
         delete_tg_grp = client.delete_target_group(
             TargetGroupArn=str(target_grp_arn)
         )
@@ -76,9 +55,10 @@ def delete_aws_resources(
         # )
         return delete_lstnr_rule,delete_tg_grp #,remove_lstnr_cert
 
-    except Exception as e:
-        message = 'FAILED: Unable to remove objects from AWS. Reason: {}'.format(e)
-        raise Exception(message)     
+    except ClientError as e:
+        message = 'Reason - {}'.format(e)
+        raise ClientError(message)
+
 
 def update_ddb_ssm(
     region,
@@ -128,48 +108,47 @@ def update_ddb_ssm(
             Type='String',
         )
 
-        return update_ddb
+        return update_ddb,update_ssm
 
-    except Exception as e:
-        message = 'FAILED: Unable to update SSM metadata. Reason: {}'.format(e)
-        raise Exception(message)
+    except ClientError as e:
+        message = 'Reason - {}'.format(e)
+        raise ClientError(message)
 
 def main():
-
-    global options
     options = get_configs()
+    option_dict = vars(options)
+    if ('') in option_dict.values():
+        message = "Empty Values Found. Nothing to delete here. Skipping... {}".format(option_dict.values())
+        print(message)
+        sys.exit(0)
+    else:
+        try:
+            remove_aws = delete_aws_resources(
+                options.lstnr_rule_arn,
+                options.target_grp_arn,
+                options.listener_arn,
+                options.cert,
+                options.region,
+                )
+            update_metadata_ddb_ssm = update_ddb_ssm(
+                options.region,
+                options.table_name,
+                options.portnum,
+                options.fqdn,
+                options.alb,
+                options.cert,
+                options.target_grp_arn,
+                options.env,
+                options.lstnr_rule_arn,
+                options.listener_arn,
+                options.state,
+                )
 
-    try:
-        check_before_delete = check_fqdn(
-            options.fqdn,
-            options.region,
-        )
-        remove_aws = delete_aws_resources(
-            options.lstnr_rule_arn,
-            options.target_grp_arn,
-            options.listener_arn,
-            options.cert,
-            options.region,
-            )
-        update_metadata_ddb_ssm = update_ddb_ssm(
-            options.region,
-            options.table_name,
-            options.portnum,
-            options.fqdn,
-            options.alb,
-            options.cert,
-            options.target_grp_arn,
-            options.env,
-            options.lstnr_rule_arn,
-            options.listener_arn,
-            options.state,
-            )
+            print(remove_aws,update_metadata_ddb_ssm)
 
-        return check_before_delete,remove_aws,update_metadata_ddb_ssm
-
-    except Exception as e:
-        message = 'FAILED: Script Failed to Execute {}'.format(e)
-        return message
+        except Exception as e:
+            message = 'FAILED: Script Failed to Execute {}'.format(e)
+            raise Exception(message)
 
 if __name__ == "__main__":
-     print(main())
+     main()
